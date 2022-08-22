5315535d962c309641380e8f174c1a32b0bc2abf
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-08-22 21:04:02 +0200

    feat(results): list added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import starWars from './img/starWars.png';
+import ListFactory from './listFactory';
 
 function App() {
 
-  const [data, setData] = useState([]);
-
-  // fetching data
-  useEffect(() => {
-    let tempoData = []
-    axios.get('https://swapi.dev/api/people/')
-      .then((resPeople) => {
-        tempoData.push(...resPeople.data.results);
-        axios.get('https://swapi.dev/api/planets/')
-          .then((resPlanets) => {
-            tempoData.push(...resPlanets.data.results);
-          })
-          .catch((err) => console.log('error fetch data planets: ' + err))
-      })
-      .then(() => {
-        setData(tempoData);
-      })
-      .catch((err) => console.log('error fetch data people: ' + err))
-  }, []);
+  const [inputValue, setInputValue] = useState('');
+  let test = 1;
+  const handleValid = (e) => {
+    e.preventDefault();
+    test = 2;
+  }
 
 
   return (
@@ -34,18 +22,15 @@ function App() {
         </div>
         <div className="App_search_searchBarButton">
           <form className="App_search_searchBarButton--form">
-            <input placeholder='Planète ou personnage' className="App_search_searchBarButton--input" />
-            <button type='submit' className="App_search_searchBarButton--button" onClick={(e) => {
-              e.preventDefault();
-              console.log(data)
-            }}>
+            <input placeholder='Planète ou personnage' className="App_search_searchBarButton--input" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+            <button type='submit' className="App_search_searchBarButton--button" onClick={(e) => handleValid(e)}>
               Rechercher
             </button>
           </form>
         </div>
       </div>
       <div className="App_results">
-
+            <ListFactory word={test}/>
       </div>
       <div>
 
